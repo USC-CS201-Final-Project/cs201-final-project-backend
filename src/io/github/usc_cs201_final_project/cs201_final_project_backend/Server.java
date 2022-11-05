60cf510833808aab5d32939f37e47f67c4ec95b9
MajikalExplosions <110285691+jliu7350@users.noreply.github.com>
2022-11-05 14:34:37 -0700

    Add libraries

--- src/io/github/usc_cs201_final_project/cs201_final_project_backend/Server.java
+++ src/io/github/usc_cs201_final_project/cs201_final_project_backend/Server.java
@@ -1,10 +1,23 @@
 package io.github.usc_cs201_final_project.cs201_final_project_backend;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.google.gson.Gson;
+
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello, world!");
+		Gson g;
+		try {
+			Connection c = DriverManager.getConnection("jdbc:mysql://localhost/__db__?user=root&password=root");
+			Statement s = c.createStatement();
+			ResultSet rs = s.executeQuery("SELECT * FROM *;");
+		} catch (SQLException e) {}
 	}
 
 }
