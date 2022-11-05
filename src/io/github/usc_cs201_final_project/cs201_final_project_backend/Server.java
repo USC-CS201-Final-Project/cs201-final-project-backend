f8c0d58cb7b0288faf468489721aa425b0298a90
MajikalExplosions <110285691+jliu7350@users.noreply.github.com>
2022-11-05 14:28:22 -0700

    Add base files

---
+++ src/io/github/usc_cs201_final_project/cs201_final_project_backend/Server.java
@@ -0,0 +1,10 @@
+package io.github.usc_cs201_final_project.cs201_final_project_backend;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
