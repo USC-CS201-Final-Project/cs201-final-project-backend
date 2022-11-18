2adb0c03e42e11007086c6ee7fc3685bc9e2a9be
Iris Li <ifli@usc.edu>
2022-11-18 12:52:10 -0800

    added stuff to player classes and game manager
    I realized I created the combatant classes after Joeseph already did, so I'm replacing his with mine, it just has more getters and setters

--- src/io/github/usc_cs201_final_project/cs201_final_project_backend/Combatant.java
+++ src/io/github/usc_cs201_final_project/cs201_final_project_backend/Combatant.java
@@ -1,22 +1,36 @@
 package io.github.usc_cs201_final_project.cs201_final_project_backend;
 
 public class Combatant {
-	private int healthCurrent;
-	private int healthMax;
+	private int maxHealth;
+	private int currentHealth;
 	private int costumeID;
-	
-	public Combatant(int cid, int health) {
-		costumeID = cid;
-		healthCurrent = health;
-		healthMax = health;
+
+	Combatant(int maxHealth, int costumeID) {
+		this.maxHealth = maxHealth;
+		this.currentHealth = maxHealth;
+		this.costumeID = costumeID;
 	}
-	
+
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+	public void setCurrentHealth(int hp) {
+		this.currentHealth = hp;
+	}
+	public int getCostumeID() {
+		return costumeID;
+	}
+	public void setCostumeID(int costumeID) {
+		this.costumeID = costumeID;
+	}
 	public void takeDamage(int damage) {
-		healthCurrent -= damage;
-	}
-	
-	public boolean isDead() {
-		return healthCurrent <= 0;
+		setCurrentHealth(currentHealth - damage);
 	}
 
 }
